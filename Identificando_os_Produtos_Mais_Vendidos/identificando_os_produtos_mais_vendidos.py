00e0a75cfac70f3d108a26c2c32f12f7b649123b
Manuela França <manuelaalves26@gmail.com>
2024-10-01 14:48:14 -0300

    Implemento da função que faz a contagem de cada produto e compara a contagem atual com a contagem máxima.

---
+++ Identificando_os_Produtos_Mais_Vendidos/identificando_os_produtos_mais_vendidos.py
@@ -0,0 +1,20 @@
+def produto_mais_vendido(produtos):
+    contagem = {}
+
+    for produto in produtos:
+        if produto in contagem:
+            contagem[produto] += 1
+        else:
+            contagem[produto] = 1
+
+    max_produto = None
+    max_count = 0
+
+    for produto, count in contagem.items():
+        if count > max_count:
+            max_count = count
+            max_produto = produto
+
+    return max_produto
+
+def obter_entrada_produto():
